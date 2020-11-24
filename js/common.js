64a500db76166ac763dbc5188180c55cbc684b2c
nickolay.grin <grinmail84@gmail.com>
2020-11-24 13:01:08 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -51,7 +51,15 @@ $(document).ready(function() {
 				prevArrow: '<div class="slick-prev slick-arrow"><i class="far fa-arrow-left"></i><div/>',
 				nextArrow: '<div class="slick-next slick-arrow"><i class="far fa-arrow-right"></i><div/>',
 				slidesToShow: 1,
-				slidesToScroll: 1
+				slidesToScroll: 1,
+				responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				adaptiveHeight: true,
+			}
+		}
+		]
 			});
 		} 
 
