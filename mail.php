fbe4d1495332f6355d18ade1987032928fc0db51
nickolay.grin <grinmail84@gmail.com>
2020-11-25 11:24:43 +0200

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,11 +1,13 @@
 <?php
 
 $recepient = "kravchenko1395@gmail.com";
-$sitename = "Алекперов";
+$sitename = "promotix";
+$nameform = trim($_POST["name_form"]);
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
+$company = trim($_POST["company"]);
 
 $pagetitle = "Новая заявка с сайта";
-$message = "name: $name \nEmail: $email ";
+$message = "Name form: $name_form \nName: $name \nEmail: $email \nCompany name: $company ";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
 ?> 
